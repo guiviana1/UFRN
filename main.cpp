777f202eeb2f49c803759b3a17276f6f56078674
Luiz Guilherme <83844853+guiviana1@users.noreply.github.com>
2024-06-06 10:29:50 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,32 +1,19 @@
-#include <bits/stdc++.h>
+int fatorial(int n) {
+  
+  int fatn;
 
-int main () {
-
-    int linhas, colunas;
-    std::cin >> linhas >> colunas;
-
-
-    int matriz1[linhas][colunas];
-    int matriz2[colunas][linhas];
-
-    for(int i = 0 ; i < linhas ; i++) {        
-        for(int j = 0 ; j < colunas ; j++) {
-            std::cin >> matriz1[i][j];
-        }
+  if (n <= 1) {
+    return 1;
+    }
+  else {
+    fatn = n * fatorial(n-1);
     }
 
-     for(int i = 0 ; i < colunas ; i++) {         
-        for(int j = 0 ; j < linhas ; j++) {
-            matriz1[linhas][colunas] = matriz2[colunas][linhas];
+  return fatn;
+}
 
-
-        }
-     }
-
-
-    if (matriz1 == matriz2) {
-        std::cout << "Matriz Simétrica";
-    }
-
-    return 0;
+int num_combinacoes(int n1, int n2) {
+  int nc;
+  nc = fatorial(n1) / (fatorial(n2) * fatorial(n1-n2));
+  return nc;
 }
